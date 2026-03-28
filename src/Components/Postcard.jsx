1a9b75e322f2a48a0461e099f4d0b05f60b59292
archit20122001 <archit20122001@gmail.com>
2026-03-28 21:55:57 +0530

    Minor UI changes

--- src/Components/Postcard.jsx
+++ src/Components/Postcard.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 function Postcard({ $id, title, featuredImage }) {
     return (
         <Link to={`/post/${$id}`}>
-            <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full bg-gray-600 hover:bg-gray-800 transition-all duration-400 rounded-xl p-4'>
                 <div className='w-full justify-center mb-4'>
                     <img src={appwriteService.getFileView(featuredImage)} alt={title}
                         className='rounded-xl' />
                 </div>
                 <h2
-                    className='text-xl font-bold'
+                    className='text-xl font-bold text-white'
                 >{title}</h2>
             </div>
         </Link>
