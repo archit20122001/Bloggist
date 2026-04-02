c91012b964b4b79ed94134c248871dffba310781
archit20122001 <archit20122001@gmail.com>
2026-04-02 12:10:59 +0530

    Image sized uniformly

--- src/Components/Postcard.jsx
+++ src/Components/Postcard.jsx
@@ -5,14 +5,17 @@ import { Link } from 'react-router-dom'
 function Postcard({ $id, title, featuredImage }) {
     return (
         <Link to={`/post/${$id}`}>
-            <div className='w-full min-h-108 bg-gray-600 hover:bg-gray-800 transition-all duration-400 rounded-xl p-4'>
-                <div className='w-full justify-center mb-4'>
-                    <img src={appwriteService.getFileView(featuredImage)} alt={title}
-                        className='rounded-xl transition-transform duration-400 ease-in-out hover:scale-105'/>
+            <div className='w-full h-full bg-gray-600 hover:bg-gray-800 transition-all duration-400 rounded-xl p-4'>
+                <div className='w-full h-48 justify-center mb-4 overflow-hidden rounded-xl'>
+                    <img
+                        src={appwriteService.getFileView(featuredImage)}
+                        alt={title}
+                        className='w-full h-full object-cover transition-transform duration-400 ease-in-out hover:scale-105'
+                    />
                 </div>
-                <h2
-                    className='text-xl font-bold text-white'
-                >{title}</h2>
+                <h2 className='text-xl font-bold text-white'>
+                    {title}
+                </h2>
             </div>
         </Link>
     )
